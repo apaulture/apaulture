e328b425dd0984dab73981a2d6f85545bbca2ce4
apaulture <apaulture@gmail.com>
2023-04-17 15:44:43 -0700

    Fixed manage.py pointer at correct project settings path for prod; included bootstrap 5.3 compiled; initialized home page; removed sqlite db; included license and readme; updated requirements with psycopg2 (non-binary)

--- apaulture/prod.py
+++ apaulture/prod.py
@@ -2,7 +2,8 @@ from .settings import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else ['*']
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
