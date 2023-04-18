f4bde736923b4ee67553b707284306a2f629b846
Paul Leelaviwatana <apaulture@gmail.com>
2023-04-18 12:04:10 -0700

    Updated wsgi app to point to correct settings module

--- apaulture/wsgi.py
+++ apaulture/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apaulture.settings')
+settings_module = 'apaulture.production' if 'WEBSITE_HOSTNAME' in os.environ else 'apaulture.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
